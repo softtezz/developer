47c5703ac85c03c7c6efb8acf4bf90164d1a3950
softtezz <36056115+softtezz@users.noreply.github.com>
2021-09-27 01:16:02 +0200

    go analytics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./App.css";
 import Main from "./containers/Main";
+import ReactGA from 'react-ga';
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
 
+//Unique Google Analytics tracking number
+ReactGA.initialize('G-LLXRRYY9EJ');
 function App() {
   return (
     <ThemeProvider theme={chosenTheme}>
       <>
         <GlobalStyles />
         <div>
+        useEffect( () => {
+
+        // This line will trigger on a route change
+        ReactGA.pageview(window.location.pathname + window.location.search); 
+
+    });
           <Main theme={chosenTheme} />
         </div>
       </>
